e20d1f3906e5f50748d2e4deb2c69fefe67a5ed7
kanepon <cindyrotus@gmail.com>
2023-06-10 08:13:11 +0900

    20230610-0813

--- operation.py
+++ operation.py
@@ -5,6 +5,10 @@ from datetime import datetime
 from decimal import Decimal
 logfile = glob.glob(b'fw/*.log.tar.gz')
 
+# Print the header
+print('# Firewall traffic log that contains socialist state, communist nation, Authoritarian political states, and so on.')
+print('# DateTime(nanosecond), src-ip, dst-ip, dst-port, dst-country')
+
 # Read the globbed log files one by one.
 for file in logfile:
 
@@ -23,31 +27,37 @@ for file in logfile:
         # Split by the country name that is socialist, communism, feudalism, state.
         if b'Russia' in mlist[1] or b'democratic' in mlist[1] or b'China' in mlist[1] or b'Cuba' in mlist[1]:
           dstcountry = mlist[1].decode('utf-8')
+
+          for item in slist:
+
+            # Get event date and time from log.
+            if b'eventtime' in item:
+              jlist = item.split(b'=')
+              epochtime = int(jlist[1].decode('utf-8'))
+              dt = datetime.fromtimestamp(epochtime // 1000000000)
+              stime = dt.strftime('%Y-%m-%d %H:%M:%S')
+              stime += '.' + str(int(epochtime % 1000000000)).zfill(9)
+              #print(stime,end='\t')
+  
+            # Get the dst ip.
+            if b'dstip' in item:
+              jlist = item.split(b'=')
+              dstip = jlist[1].decode('utf-8')
+              #print(type(dstip))
+  
+            # Get the dst ip.
+            if b'srcip' in item:
+              jlist = item.split(b'=')
+              srcip = jlist[1].decode('utf-8')
+            
+            # Get the dst port
+            if b'dstport' in item:
+              jlist = item.split(b'=')
+              dstport = jlist[1].decode('utf-8')
+        
         else:
           continue
 
-        for item in slist:
-
-          # Get event date and time from log.
-          if b'eventtime' in item:
-            jlist = item.split(b'=')
-            epochtime = int(jlist[1].decode('utf-8'))
-            dt = datetime.fromtimestamp(epochtime // 1000000000)
-            stime = dt.strftime('%Y-%m-%d %H:%M:%S')
-            stime += '.' + str(int(epochtime % 1000000000)).zfill(9)
-            #print(stime,end='\t')
-
-          # Get the dst ip.
-          if b'dstip' in item:
-            jlist = item.split(b'=')
-            dstip = jlist[1].decode('utf-8')
-            #print(type(dstip))
-
-          # Get the dst ip.
-          if b'srcip' in item:
-            jlist = item.split(b'=')
-            srcip = jlist[1].decode('utf-8')
-
         # Collected items are compiled and output to the console.
-        print("{},{},{},{}".format(stime, srcip, dstip, dstcountry))
+        print("{},{},{},{},{}".format(stime, srcip, dstip, dstport, dstcountry))
 
