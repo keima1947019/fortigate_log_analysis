a6b40a49036ee7808a2ec607842292f79f6fd321
kanepon <cindyrotus@gmail.com>
2023-06-13 09:19:35 +0900

    20230613-0919

--- count_country.py
+++ count_country.py
@@ -40,8 +40,7 @@ for file in fglog:
         else:
           dstcnt[ctry] = 1
 
-################################################################################################################################
-
+# for development, sample data
 dstcnt = {'Japan': 378806, 'United States': 236004, 'India': 13700, 'United Kingdom': 3258, 'Taiwan': 5754, 'Canada': 1026, 'Switzerland': 940, 'Germany': 4006, 'Australia': 3585, 'Singapore': 5515, 'France': 861, 'Netherlands': 986, 'Hong Kong': 3159, 'Russian Federation': 184, 'Spain': 1743, 'Ireland': 503, 'Finland': 143, 'Colombia': 4, 'Norway': 4, 'Italy': 6, 'Korea, Republic of': 1113, 'Austria': 18, 'Sweden': 21, 'Mexico': 3, 'Malaysia': 4, 'Luxembourg': 2, 'Denmark': 73, 'Belgium': 137, 'Brazil': 55, 'China': 67, 'Bulgaria': 3, 'Hungary': 12, 'United Arab Emirates': 5, 'Turkey': 3, 'Czech Republic': 11, 'Chile': 1, 'Argentina': 6, 'Ukraine': 2, 'Panama': 2, 'Reserved': 7, 'Indonesia': 4, 'New Zealand': 2, 'Poland': 4, 'South Africa': 4}
 
 # pie chart
@@ -83,21 +82,21 @@ df = pd.read_csv('output.csv')
 os.remove('output.csv')
 
 fig = px.bar(df, x = 'country', y = 'count' , title = 'fw-traffic')
-fig.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[0,400000]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
+fig.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[12000,400000]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
 fig.update_layout(xaxis={'categoryorder':'total descending'})
-fig.write_image("access-country-bar.png")
+fig.write_image("access-country-bar-0.png")
 
 fig1 = px.bar(df, x = 'country', y = 'count' , title = 'fw-traffic')
-fig1.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[0,20]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
+fig1.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[3000,6000]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
 fig1.update_layout(xaxis={'categoryorder':'total descending'})
-fig1.write_image("access-country-bar-mini.png")
+fig1.write_image("access-country-bar-1-.png")
 
 fig2 = px.bar(df, x = 'country', y = 'count' , title = 'fw-traffic')
-fig2.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[0,15000]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
+fig2.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[200,1800]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
 fig2.update_layout(xaxis={'categoryorder':'total descending'})
-fig2.write_image("access-country-bar-middle-big.png")
+fig2.write_image("access-country-bar-2.png")
 
 fig3 = px.bar(df, x = 'country', y = 'count' , title = 'fw-traffic')
-fig.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[0,5000]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
+fig3.update_layout(width=1900,height=1000,margin=dict(l=50,r=50,b=40,t=60),yaxis=dict(range=[0,200]),xaxis=dict(dtick=1, autorange=True),title=dict(font = dict(size=20)))
 fig3.update_layout(xaxis={'categoryorder':'total descending'})
-fig3.write_image("access-country-bar-middle-middle.png")
+fig3.write_image("access-country-bar-3.png")
